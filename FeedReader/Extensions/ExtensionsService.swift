d509de1aaedc81bc9cb518a8ba23e2b4a817a253
Stan Gajda <stgajda@gmail.com>
2021-07-21 02:01:41 +0300

    retain cykle set to weak

--- FeedReader/Extensions/ExtensionsService.swift
+++ FeedReader/Extensions/ExtensionsService.swift
@@ -28,6 +28,14 @@ extension Publisher {
     }
 }
 
+extension Publisher where Failure == Never {
+    func assign<Root: AnyObject>(to keyPath: ReferenceWritableKeyPath<Root, Output>, on root: Root) -> AnyCancellable {
+       sink { [weak root] in
+            root?[keyPath: keyPath] = $0
+        }
+    }
+}
+
 extension URLRequest {
     func get() -> URLRequest {
         var copy = self
