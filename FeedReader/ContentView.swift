cd5838382d4b4e5206a20f5a1681a32ec0fd0b8e
Stan Gajda <stgajda@gmail.com>
2021-07-24 00:54:22 +0300

    removed preview from content

--- FeedReader/ContentView.swift
+++ FeedReader/ContentView.swift
@@ -14,9 +14,3 @@ struct ContentView: View {
         MoviesListView(viewModel: viewModel)
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
