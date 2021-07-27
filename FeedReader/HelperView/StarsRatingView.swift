dc9ffad93232a072773116c053e058268ceb0007
Stan Gajda <stgajda@gmail.com>
2021-07-27 22:06:12 +0300

    movie details hardcoded and stars view added

---
+++ FeedReader/HelperView/StarsRatingView.swift
@@ -0,0 +1,36 @@
+//
+//  StarsRatingView.swift
+//  FeedReader
+//
+//  Created by Stan Gajda on 27/07/2021.
+//
+
+import SwiftUI
+
+struct StarsRatingView: View {
+    var rating: Double
+    var maxRating: Int
+
+    var body: some View {
+        let stars = HStack(spacing: 2) {
+            ForEach(0..<maxRating) { _ in
+                Image(systemName: "star.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+        }
+
+        stars.overlay(
+            GeometryReader { geometry in
+                let width = CGFloat(rating) / CGFloat(maxRating) * geometry.size.width
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .frame(width: width)
+                        .foregroundColor(.yellow)
+                }
+            }
+            .mask(stars)
+        )
+        .foregroundColor(.gray)
+    }
+}
