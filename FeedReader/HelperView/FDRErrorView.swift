a70d14eb95c11f6b9015646086e34dfc0afa7a4f
Stan Gajda <stgajda@gmail.com>
2021-08-04 22:40:21 +0300

    error view styles improved

--- FeedReader/HelperView/FDRErrorView.swift
+++ FeedReader/HelperView/FDRErrorView.swift
@@ -13,12 +13,15 @@ struct FDRErrorView: View {
     var body: some View {
         VStack {
             Text("An Error Occured")
-                .font(.title)
+                .font(.headline)
+                .minimumScaleFactor(0.5)
+                .lineLimit(2)
+                .padding(.bottom)
             Text(error.localizedDescription)
                 .font(.callout)
-                .multilineTextAlignment(.center)
-                .padding(.bottom, 40)
-                .padding()
+                .minimumScaleFactor(0.5)
+                .lineLimit(2)
+                .padding(.bottom)
         }
     }
 }
@@ -27,7 +30,7 @@ struct FDRErrorView: View {
 struct FDRErrorView_Previews: PreviewProvider {
     static var previews: some View {
         FDRErrorView(error: NSError(domain: "", code: 0, userInfo: [
-            NSLocalizedDescriptionKey: "Something went wrong"]))
+                                        NSLocalizedDescriptionKey: "Something went wrong"])).frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
     }
 }
 #endif
