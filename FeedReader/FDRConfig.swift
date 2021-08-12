02c856a90e97799cc2b3e9a03c9af4602da89829
Stan Gajda <stgajda@gmail.com>
2021-08-12 04:05:23 +0300

    swap enum with url builder

--- FeedReader/FDRConfig.swift
+++ FeedReader/FDRConfig.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 
+struct FDRAPIUrlBuilder: FDRAPIUrlBuilderProtocol {
+    static var baseURL: URL? { URL(string: "https://api.themoviedb.org/") }
+    static var imageURL: URL? { URL(string: "https://image.tmdb.org/t/p/original") }
+    static var prefix: String { "3" }
+    static var apiKey: String { "babcada8d42a5fd4857231c42240debd" }
+}
+
 enum Path{
     case trending
     case movieDetail(_ id: Int)
@@ -18,13 +25,6 @@ enum Path{
             return "movie/\(String(id))"
         }
     }
-}
-
-struct FDRAPIUrlBuilder: FDRAPIUrlBuilderProtocol {
-    static var baseURL: URL? { URL(string: "https://api.themoviedb.org/") }
-    static var imageURL: URL? { URL(string: "https://image.tmdb.org/t/p/original") }
-    static var prefix: String { "3" }
-    static var apiKey: String { "babcada8d42a5fd4857231c42240debd" }
 }
 
 extension URLSession{
