f736b44fe1df8f4200efc372c6ab5ab8e1da1c46
Stan Gajda <stgajda@gmail.com>
2021-08-14 03:58:37 +0300

    cache configuration

--- FeedReader/FDRConfig.swift
+++ FeedReader/FDRConfig.swift
@@ -18,16 +18,19 @@ let API_IMAGE_ORIGINAL_PATH = "original"
 let API_IMAGE_W200_PATH = "w200"
     
 let CACHE_TOTAL_COST_LIMIT = 50_000_000
-
+let CACHE_PATH = "DownloadCache"
 
 extension URLSession{
     static func configuredURLSession() -> URLSession {
         let configuration = URLSessionConfiguration.default
+        let cachesURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
+        let diskCacheURL = cachesURL.appendingPathComponent(CACHE_PATH)
+        let urlCache = URLCache(memoryCapacity: 50_000_000, diskCapacity: 100_000_000, directory: diskCacheURL)
         configuration.waitsForConnectivity = true
         configuration.timeoutIntervalForRequest = 60
         configuration.timeoutIntervalForResource = 180
         configuration.httpMaximumConnectionsPerHost = 7
-        configuration.urlCache = .shared
+        configuration.urlCache = urlCache
         configuration.requestCachePolicy = .useProtocolCachePolicy
         return URLSession(configuration: configuration)
     }
