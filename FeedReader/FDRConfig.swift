2152c643b585cb20ed889e157edb26df202a8e25
Stan Gajda <stgajda@gmail.com>
2021-08-14 04:02:09 +0300

    path removed from cache directory

--- FeedReader/FDRConfig.swift
+++ FeedReader/FDRConfig.swift
@@ -18,14 +18,11 @@ let API_IMAGE_ORIGINAL_PATH = "original"
 let API_IMAGE_W200_PATH = "w200"
     
 let CACHE_TOTAL_COST_LIMIT = 50_000_000
-let CACHE_PATH = "DownloadCache"
 
 extension URLSession{
     static func configuredURLSession() -> URLSession {
         let configuration = URLSessionConfiguration.default
-        let cachesURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
-        let diskCacheURL = cachesURL.appendingPathComponent(CACHE_PATH)
-        let urlCache = URLCache(memoryCapacity: 50_000_000, diskCapacity: 100_000_000, directory: diskCacheURL)
+        let urlCache = URLCache(memoryCapacity: 50_000_000, diskCapacity: 100_000_000)
         configuration.waitsForConnectivity = true
         configuration.timeoutIntervalForRequest = 60
         configuration.timeoutIntervalForResource = 180
