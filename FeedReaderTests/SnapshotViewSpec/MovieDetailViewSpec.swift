f90fd4b0ebe2568160b171af45f79df9a41d1d1a
Stan <stgajda@gmail.com>
2023-07-06 17:07:02 +0200

    snapshot draft

---
+++ FeedReaderTests/SnapshotViewSpec/MovieDetailViewSpec.swift
@@ -0,0 +1,41 @@
+//
+//  MovieDetailViewSpec.swift
+//  FeedReaderTests
+//
+//  Created by Stan Gajda on 06/07/2023.
+//
+
+import Foundation
+@testable import FeedReader
+import SwiftUI
+import Nimble
+import Quick
+import SnapshotTesting
+
+class MovieDetailViewSpec: QuickSpec {
+  override func spec() {
+    describe("describe") {
+        beforeEach {
+            
+        }
+        
+        afterEach {
+            
+        }
+        
+        context("context") {
+            beforeEach {
+                
+            }
+            
+            it("it 1") {
+             
+            }
+
+            it("it 2") {
+             
+            }
+        }
+    }
+  }
+}
