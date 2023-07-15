4f2c44b3bfa96b034ab273362d955636e5d49c6c
Stan <stgajda@gmail.com>
2023-07-15 21:55:37 +0200

    assembly and injection name moved to separate file

---
+++ FeedReader/DependencyInjection/Assembly.swift
@@ -0,0 +1,166 @@
+//
+//  Assembly.swift
+//  FeedReader
+//
+//  Created by Stan Gajda on 15/07/2023.
+//
+
+import Foundation
+import Swinject
+
+// MARK:- Session
+class NetworkAssembly: AssemblyProtocol {
+    func assemble(container: Container) {
+        container.register(URLSessionProtocol.self) { _ in
+            URLSession.configuredURLSession()
+        }
+    }
+}
+
+// MARK:- Service
+class ServiceAssembly: AssemblyProtocol {
+    func assemble(container: Container) {
+        container.register(ServiceProtocol.self) { resolver in
+            Service()
+        }
+        
+        container.register(MovieListServiceProtocol.self) { resolver in
+            MovieListService()
+        }
+        
+        container.register(MovieDetailServiceProtocol.self) { resolver in
+            MovieDetailService()
+        }
+        
+        container.register(ImageServiceProtocol.self) { resolver in
+            ImageService()
+        }
+    }
+}
+
+// MARK:- Mock Service
+class MockServiceAssembly: AssemblyProtocol {
+    func assemble(container: Container) {
+        container.register(MovieListServiceProtocol.self) { resolver in
+            MockMovieListService()
+        }
+
+        container.register(MovieDetailServiceProtocol.self) { resolver in
+            MockMovieDetailService()
+        }
+
+        container.register(ImageServiceProtocol.self) { resolver in
+            MockImageService()
+        }
+    }
+}
+
+// MARK:- ViewModel
+class ViewModelAssembly: AssemblyProtocol {
+    func assemble(container: Container) {
+        container.register(AnyMoviesListViewModelProtocol.self) { _ in
+            MoviesListViewModel()
+                .eraseToAnyViewModelProtocol()
+        }
+        
+        container.register(AnyMovieDetailViewModelProtocol.self) { resolver , movie in
+            MovieDetailViewModel.instance(movie)
+                .eraseToAnyViewModelProtocol()
+        }
+        
+        container.register(AnyImageViewModelProtocol.self) { resolver , imagePath, imageSizePath in
+            ImageViewModel.instance(imagePath: imagePath, imageSizePath: imageSizePath)
+                .eraseToAnyViewModelProtocol()
+        }
+        
+        container.register(AnyImageViewModelProtocol.self) { resolver , imagePath, imageSizePath, cache in
+            ImageViewModel.instance(imagePath: imagePath, imageSizePath: imageSizePath, cache: cache)
+                .eraseToAnyViewModelProtocol()
+        }
+    }
+}
+
+// MARK:- Preview
+class MockMoviesListViewModelAssembly: AssemblyNameProtocol {
+    func assemble(container: Container) {
+        container.register(AnyMoviesListViewModelProtocol.self) { resolver in
+            MockMoviesListViewModel(.loaded)
+                .eraseToAnyViewModelProtocol()
+        }
+        
+        register(AnyMoviesListViewModelProtocol.self, container: container, name: .movieListStateLoaded) { resolver in
+            MockMoviesListViewModel(.loaded)
+                .eraseToAnyViewModelProtocol()
+        }
+
+        register(AnyMoviesListViewModelProtocol.self, container: container, name: .movieListStateLoading) { resolver in
+            MockMoviesListViewModel(.loading)
+                .eraseToAnyViewModelProtocol()
+        }
+
+        register(AnyMoviesListViewModelProtocol.self, container: container, name: .movieListStateFailed) { resolver in
+            MockMoviesListViewModel(.failedLoaded)
+                .eraseToAnyViewModelProtocol()
+        }
+        
+        register(AnyMovieDetailViewModelProtocol.self, container: container) { resolver, movie in
+            MockMovieDetailViewModel(movie)
+                .eraseToAnyViewModelProtocol()
+        }
+    }
+}
+
+class MockMovieDetailViewModelAssembly: AssemblyNameProtocol {
+    func assemble(container: Container) {
+        container.register(AnyMovieDetailViewModelProtocol.self) { resolver in
+            MockMovieDetailViewModel(.loaded, MoviesListViewModel.MovieItem.mock)
+                .eraseToAnyViewModelProtocol()
+        }
+        
+        register(AnyMovieDetailViewModelProtocol.self, container: container, name: .movieDetailStateLoaded) { resolver in
+            MockMovieDetailViewModel(.loaded, MoviesListViewModel.MovieItem.mock)
+                .eraseToAnyViewModelProtocol()
+        }
+
+        register(AnyMovieDetailViewModelProtocol.self, container: container, name: .movieDetailStateLoading) { resolver in
+            MockMovieDetailViewModel(.loading, MoviesListViewModel.MovieItem.mock)
+                .eraseToAnyViewModelProtocol()
+        }
+
+        register(AnyMovieDetailViewModelProtocol.self, container: container, name: .movieDetailStateFailed) { resolver in
+            MockMovieDetailViewModel(.failedLoaded, MoviesListViewModel.MovieItem.mock)
+                .eraseToAnyViewModelProtocol()
+        }
+    }
+}
+
+class MockImageViewModelAssembly: AssemblyProtocol {
+    func assemble(container: Container) {
+        container.register(AnyImageViewModelProtocol.self) { resolver , imagePath, imageSizePath in
+            MockImageViewModel(imagePath: imagePath, imageSizePath: imageSizePath)
+                .eraseToAnyViewModelProtocol()
+        }
+        container.register(AnyImageViewModelProtocol.self) { resolver , imagePath, imageSizePath, cache in
+            MockImageViewModel(imagePath: imagePath, imageSizePath: imageSizePath, cache: cache)
+                .eraseToAnyViewModelProtocol()
+        }
+    }
+    
+}
+
+class MockImageViewModelItemDetailAssembly: AssemblyProtocol {
+    func assemble(container: Container) {
+        container.register(AnyImageViewModelProtocol.self) { resolver , imagePath, imageSizePath, cache in
+            MockImageViewModelDetail(imagePath: imagePath, imageSizePath: imageSizePath, cache: cache)
+                .eraseToAnyViewModelProtocol()
+        }
+    }
+}
+
+class MockNetworkAssembly: AssemblyProtocol {
+    func assemble(container: Container) {
+        container.register(URLSessionProtocol.self) { _ in
+            URLSession.mockURLSession()
+        }.inObjectScope(.container)
+    }
+}
---
+++ FeedReader/DependencyInjection/InjectionName.swift
@@ -0,0 +1,24 @@
+//
+//  InjectionName.swift
+//  FeedReader
+//
+//  Created by Stan Gajda on 15/07/2023.
+//
+
+import Foundation
+
+// MARK:- Injection.Name
+extension Injection.Name {
+    static let movieListStateLoaded = Self("MovieListStateLoaded")
+    static let movieListStateLoading = Self("MovieListStateLoading")
+    static let movieListStateFailed = Self("MovieListStateFailed")
+    
+    static let movieDetailStateLoaded = Self("MovieDetailStateLoaded")
+    static let movieDetailStateLoading = Self("MovieDetailStateLoading")
+    static let movieDetailStateFailed = Self("MovieDetailStateFailed")
+    
+    static let imageStateLoaded = Self("ImageStateLoaded")
+
+    static let itemList = Self("ItemList")
+    static let itemDetail = Self("ItemDetail")
+}
