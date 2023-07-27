229304a53e19ef0c81525e39b56fe07c0cedde5e
Stan <stgajda@gmail.com>
2023-07-27 12:52:26 +0200

    swinject recursive lock removed

--- FeedReader/DependencyInjection/DIWrappers.swift
+++ FeedReader/DependencyInjection/DIWrappers.swift
@@ -41,27 +41,3 @@ import Foundation
         mutating set { self = newValue }
     }
 }
-
-// MARK:- Lock
-extension Injection {
-    fileprivate static let lock = SwinjectRecursiveLock()
-}
-
-// MARK:- SwinjectRecursiveLock
-fileprivate final class SwinjectRecursiveLock {
-    init() {
-        pthread_mutexattr_init(&recursiveMutexAttr)
-        pthread_mutexattr_settype(&recursiveMutexAttr, PTHREAD_MUTEX_RECURSIVE)
-        pthread_mutex_init(&recursiveMutex, &recursiveMutexAttr)
-    }
-    @inline(__always)
-    final func lock() {
-        pthread_mutex_lock(&recursiveMutex)
-    }
-    @inline(__always)
-    final func unlock() {
-        pthread_mutex_unlock(&recursiveMutex)
-    }
-    private var recursiveMutex = pthread_mutex_t()
-    private var recursiveMutexAttr = pthread_mutexattr_t()
-}
